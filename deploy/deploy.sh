5af3a625dc13a5469f03a7d3a34ad07b0b722eef
dogyu <dydqud@gmail.com>
2020-09-18 23:11:17 +0900

    deploy.sh

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -2,18 +2,12 @@
 
 FR_DIR=/usr/share/nginx/html
 REP_DIR=/home/ec2-user/app/step2/zip/dist
-PROJECT_NM=yureka_frontend
 
-cd $REP_DIR
+echo "> FR_DIR File Del *"
+rm -rf $FR_DIR
 
-echo "> Frontend Dir Move"
-
-cd $FR_DIR
-
-echo "> Reopritory File Copy to Frontend"
-
-sudo cp -r $REP_DIR/$PROJECT_NM/* $FR_DIR/
+echo "> File Copy to Frontend"
+sudo cp -r $REP_DIR/* $FR_DIR/
 
 echo "> Frontend Dir Confirm $FR_DIR"
-
 ls -l $FR_DIR
